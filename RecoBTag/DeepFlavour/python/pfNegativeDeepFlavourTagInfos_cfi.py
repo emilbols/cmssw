cf7f30178454f2189f10f04f7a36c946cc7525c7
Emil Bols <emilbols@hotmail.com>
2018-04-03 13:16:27 +0200

    Trying to make git work

--- RecoBTag/DeepFlavour/python/pfNegativeDeepFlavourTagInfos_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from RecoBTag.DeepFlavour.pfDeepFlavourTagInfos_cfi import pfDeepFlavourTagInfos
-
-pfNegativeDeepFlavourTagInfos = pfDeepFlavourTagInfos.clone(
-        shallow_tag_infos = cms.InputTag('pfDeepCSVNegativeTagInfos'),
-        flip = cms.bool(True)
-        )
